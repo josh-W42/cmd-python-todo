8a6cb1fa14bf8c0cb5ea44a0f0ff797c3f87dae9
josh-W42 <wilson.jd022@gmail.com>
2023-11-01 18:03:15 -0700

    updated todo class to manage a local file for todos, added exit method, added show and exit method  to application loop

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ from todo.todo import Todo
 if __name__ == '__main__':
     app = Todo()
     app.show()
-    app.add("Make Pancakes")
-    app.add(["Laugh", "Live", "Love"])
-    app.show()
+    app.start()
+
 
 
 
--- todo/todo.py
+++ todo/todo.py
@@ -6,15 +6,37 @@ class Todo:
     Class todo allows you to set up a todo app.
     """
     __todos = ["Code in python", "Get a job", "Profit"]
+    __storage_file_path = "todos.txt"
 
-    def __int__(self):
-        pass
+    def __int__(self) -> None:
+        try:
+            self.__todos = self.__get_todos(self.__storage_file_path)
+        except FileNotFoundError:
+            self.__store_todos(self.__storage_file_path, self.__todos)
+        finally:
+            print("Sync Complete...")
 
     @staticmethod
-    def __get_todos(filepath: str) -> List[str]:
-        with open(filepath, "r") as localeFile:
-            todos_local = localeFile.readlines()
+    def __get_todos(self, filepath: str) -> List[str]:
+        """
+        Opens a file storing todos and retrieves them.
+        :param filepath: The path to the file.
+        :return: A list of the stored todos.
+        """
+        with open(filepath, "r") as local_file:
+            todos_local = local_file.readlines()
         return todos_local
+
+    @staticmethod
+    def __store_todos(filepath: str, todos: List[str], ) -> None:
+        """
+        Opens a file for appending and writes a todo line by line.
+        :param filepath: Desired path to todo file.
+        :param todos: A list of todos in str format to be added.
+        """
+        with open(filepath, "a") as local_file:
+            for todo in todos:
+                local_file.write(f"{todo}\n")
 
     def add(self, items: Union[List[str], str]) -> None:
         """
@@ -37,14 +59,28 @@ class Todo:
         for i, todo in enumerate(self.__todos):
             print(f'{i + 1}. {todo}')
 
+    def exit(self) -> None:
+        """
+        Should be called when the program exits.
+        Stores todos in file and cleans up.
+        """
+        self.__store_todos(self.__storage_file_path, self.__todos)
+
     def start(self) -> None:
         """
         Starts the todo list run loop.
         :return: None
         """
         exit_condition = 'exit'
-        user_input = input("Type add, show, edit, complete, or exit: ")
+        user_input = ""
         while user_input != exit_condition:
+            user_input = input("Type add, show, edit, complete, or exit: ")
             user_input = user_input.strip()
             if user_input.startswith('add'):
                 pass
+            elif user_input.startswith('show'):
+                self.show()
+            elif user_input.startswith(exit_condition):
+                self.exit()
+                break
+
