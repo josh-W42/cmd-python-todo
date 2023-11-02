725b2d52d434a69c610c3bc721d376c9794df46d
josh-W42 <wilson.jd022@gmail.com>
2023-11-02 10:11:06 -0700

    Add now properly works from cmd line, fixed an issue related to the storing of todos, moved some variables to be constant private variables.

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from todo.todo import Todo
 
 if __name__ == '__main__':
     app = Todo()
-    app.show()
     app.start()
 
 
--- todo/todo.py
+++ todo/todo.py
@@ -5,26 +5,27 @@ class Todo:
     """
     Class todo allows you to set up a todo app.
     """
-    __todos = ["Code in python", "Get a job", "Profit"]
-    __storage_file_path = "todos.txt"
+    __todos = []
+    __STORAGE_FILE_PATH = "todos.txt"
+    __EXIT_CONDITION = 'exit'
 
-    def __int__(self) -> None:
+    def __init__(self):
         try:
-            self.__todos = self.__get_todos(self.__storage_file_path)
+            self.__todos = self.__get_todos(self.__STORAGE_FILE_PATH)
         except FileNotFoundError:
-            self.__store_todos(self.__storage_file_path, self.__todos)
+            self.__store_todos(self.__STORAGE_FILE_PATH, self.__todos)
         finally:
             print("Sync Complete...")
 
     @staticmethod
-    def __get_todos(self, filepath: str) -> List[str]:
+    def __get_todos(filepath: str) -> List[str]:
         """
         Opens a file storing todos and retrieves them.
         :param filepath: The path to the file.
         :return: A list of the stored todos.
         """
         with open(filepath, "r") as local_file:
-            todos_local = local_file.readlines()
+            todos_local = [todo.rstrip('\n') for todo in local_file]
         return todos_local
 
     @staticmethod
@@ -34,9 +35,19 @@ class Todo:
         :param filepath: Desired path to todo file.
         :param todos: A list of todos in str format to be added.
         """
-        with open(filepath, "a") as local_file:
+        with open(filepath, "w") as local_file:
             for todo in todos:
                 local_file.write(f"{todo}\n")
+
+    @staticmethod
+    def __parse_for_adding(user_input: str) -> Union[List[str], str]:
+        """
+        Parses incoming data from a user that will be used with the add method
+        :param user_input: input from a user prompt.
+        :return: Either one new todo or a list of new todos.
+        """
+        todos = user_input[4:].split(",")
+        return [todo.strip() for todo in todos]
 
     def add(self, items: Union[List[str], str]) -> None:
         """
@@ -64,23 +75,25 @@ class Todo:
         Should be called when the program exits.
         Stores todos in file and cleans up.
         """
-        self.__store_todos(self.__storage_file_path, self.__todos)
+        self.__store_todos(self.__STORAGE_FILE_PATH, self.__todos)
 
     def start(self) -> None:
         """
         Starts the todo list run loop.
         :return: None
         """
-        exit_condition = 'exit'
         user_input = ""
-        while user_input != exit_condition:
+        while user_input != self.__EXIT_CONDITION:
             user_input = input("Type add, show, edit, complete, or exit: ")
             user_input = user_input.strip()
-            if user_input.startswith('add'):
-                pass
+            if user_input.rfind('add ', 0, 4) == 0:
+                todos = self.__parse_for_adding(user_input)
+                self.add(todos)
             elif user_input.startswith('show'):
                 self.show()
-            elif user_input.startswith(exit_condition):
+            elif user_input.startswith(self.__EXIT_CONDITION):
                 self.exit()
                 break
+            else:
+                print("Usage:\nadd [todo], [todo]\nshow\nedit [number] [revised todo]\ncomplete [number]\nexit")
 
